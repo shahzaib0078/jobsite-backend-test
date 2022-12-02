8ed19d71d620ee5fd9e7f7d01f576925aa3857f4
shahzaib <m.shahzaib@fiveriverstech.com>
2022-12-02 17:45:42 +0500

    add packages

--- project/settings.py
+++ project/settings.py
@@ -140,7 +140,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, STATIC_URL)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
